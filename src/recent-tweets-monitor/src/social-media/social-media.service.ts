40be09f9d357b52611270b358ad95b3993c2dd69
Jonas <jonasbraga2001@gmail.com>
2024-11-05 22:05:07 -0300

    feat(service): :sparkles: improving tweet stream handling
    adding chunk concatenation, logging and general fixes

--- src/recent-tweets-monitor/src/social-media/social-media.service.ts
+++ src/recent-tweets-monitor/src/social-media/social-media.service.ts
@@ -2,7 +2,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { SocialMediaRepository } from './social-media.repository';
 import { SocialMediaData } from './interfaces/social-media-data.interface';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { TwitterAdapter } from './adapters/twitter.adapter';
 
 @Injectable()
@@ -10,34 +10,35 @@ export class SocialMediaService {
   private readonly logger = new Logger(SocialMediaService.name);
   private reconnectAttempts = 0;
   private maxReconnectAttempts = 5;
+  private options: { limit: number; frequency: number; maxTweets: number };
+  private dataBuffer = '';
+  private BATCH_SIZE = 25;
 
   constructor(private readonly repository: SocialMediaRepository) {}
 
-  async consumeTweets(hashtag: string): Promise<void> {
+  async consumeTweets(
+    hashtag: string,
+    options: { limit: number; frequency: number; maxTweets: number },
+  ): Promise<void> {
+    this.options = options;
     await this.connectToStream(hashtag);
   }
 
   private async connectToStream(hashtag: string): Promise<void> {
     try {
       const response = await axios.get(
-        `http://x-api-mock-server:3000/tweets/search/stream/${hashtag}`,
+        `${process.env.PUBLISHER_ENDPOINT!}/tweets/search/stream/${hashtag}`,
         {
+          params: this.options,
           responseType: 'stream',
           // timeout: 60000, // Timeout to handle idle connections
         },
       );
 
       const tweetsBuffer: Omit<SocialMediaData, 'provider'>[] = [];
-      const batchSize = 25;
-      let counter = 0;
 
       // Event Handlers
-      const onData = this.handleDataEvent.bind(
-        this,
-        tweetsBuffer,
-        batchSize,
-        counter,
-      );
+      const onData = this.handleDataEvent.bind(this, tweetsBuffer, hashtag);
       const onError = this.handleErrorEvent.bind(
         this,
         tweetsBuffer,
@@ -61,32 +62,43 @@ export class SocialMediaService {
 
   private async handleDataEvent(
     tweetsBuffer: Omit<SocialMediaData, 'provider'>[],
-    batchSize: number,
-    tweets: any,
+    hashtag: string,
+    tweetsChunk: Buffer,
   ): Promise<void> {
     try {
-      const tweetData = tweets.map(TwitterAdapter.toSocialMediaData);
+      this.logger.log(`Stream data received: ${tweetsChunk.length} bytes`);
+      // Accumulate incoming chunks
+      this.dataBuffer += tweetsChunk.toString();
 
-      tweetsBuffer.push(...tweetData);
+      // Extract complete JSON arrays from the dataBuffer, this is usually needed because sometimes the data is not received in full JSON arrays, but cut off in the middle
+      const tweets = this.extractCompleteArraysFromBuffer();
+      this.logger.log(`Extracted ${tweets.length} tweets from last chunk`);
 
-      if (tweetsBuffer.length >= batchSize) {
-        await this.addTweets(tweetsBuffer);
-        tweetsBuffer.length = 0;
+      // Process each tweet received from the stream data in batches
+      if (tweets.length > 0) {
+        const tweetData = tweets.map(TwitterAdapter.toSocialMediaData);
+        tweetsBuffer.push(...tweetData);
+
+        if (tweetsBuffer.length >= this.BATCH_SIZE) {
+          await this.addTweets(tweetsBuffer, hashtag);
+          tweetsBuffer.length = 0;
+          this.logger.log('Buffer cleared');
+        }
       }
     } catch (error) {
-      this.logger.error('Error parsing tweet data:', error);
+      this.logger.error('Error processing tweet data:', error);
     }
   }
 
   private async handleErrorEvent(
     tweetsBuffer: Omit<SocialMediaData, 'provider'>[],
     hashtag: string,
-    response: any,
+    response: AxiosResponse,
     error: any,
   ): Promise<void> {
     this.logger.error('Stream error:', error);
     response.data.destroy();
-    await this.handleStreamEnd(tweetsBuffer, hashtag);
+    await this.handleStreamEnd(tweetsBuffer, hashtag, true);
   }
 
   private async handleEndEvent(
@@ -100,14 +112,16 @@ export class SocialMediaService {
   private async handleStreamEnd(
     tweetsBuffer: Omit<SocialMediaData, 'provider'>[],
     hashtag: string,
+    reconnect: boolean = false,
   ): Promise<void> {
     // Save any remaining tweets
     if (tweetsBuffer.length > 0) {
-      await this.addTweets(tweetsBuffer);
+      this.logger.log(`Saving ${tweetsBuffer.length} remaining tweets...`);
+      await this.addTweets(tweetsBuffer, hashtag);
       tweetsBuffer.length = 0;
     }
     // Attempt to reconnect
-    await this.reconnectWithBackoff(hashtag);
+    reconnect && (await this.reconnectWithBackoff(hashtag));
   }
 
   private async reconnectWithBackoff(hashtag: string): Promise<void> {
@@ -123,18 +137,49 @@ export class SocialMediaService {
   }
 
   private calculateBackoffDelay(attempt: number): number {
-    const baseDelay = 1000; // 1 second
+    const baseDelay = 1000;
     const delay = baseDelay * Math.pow(2, attempt) + Math.random() * 1000;
     return delay;
   }
 
   async addTweets(
     dataArray: Omit<SocialMediaData, 'provider'>[],
+    hashtag: string,
   ): Promise<void> {
     const dataWithProvider = dataArray.map((data) => ({
       ...data,
       provider: 'twitter',
     }));
-    await this.repository.batchCreate(dataWithProvider);
+    this.logger.log(`Saving ${dataWithProvider.length} tweets...`);
+    await this.repository.batchInsertion(dataWithProvider, hashtag);
+  }
+
+  private extractCompleteArraysFromBuffer(): any[] {
+    const tweetsList = [];
+    const arrayRegex = /\[.*?\]/gs;
+    let match;
+
+    // Extract all complete JSON arrays (everything inside brackets []) from the buffer
+    while ((match = arrayRegex.exec(this.dataBuffer)) !== null) {
+      const jsonString = match[0];
+
+      try {
+        const parsedArray = JSON.parse(jsonString);
+        tweetsList.push(...parsedArray);
+
+        // Remove the processed part from the buffer
+        this.dataBuffer = this.dataBuffer.slice(
+          match.index + jsonString.length,
+        );
+
+        // Reset regex lastIndex since dataBuffer has changed
+        arrayRegex.lastIndex = 0;
+      } catch (e) {
+        // Parsing failed; likely incomplete JSON
+        break; // Wait for more data in the next chunk
+      }
+    }
+
+    return tweetsList;
   }
 }
